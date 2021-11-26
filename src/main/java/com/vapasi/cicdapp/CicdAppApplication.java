9daae060e4b697ea68d43607bb20e507eab14d1d
Amit Kumar <amit.kumar.thoughtworks@aegonlife.com>
2021-11-26 13:55:52 +0530

    Initial springboot project

---
+++ src/main/java/com/vapasi/cicdapp/CicdAppApplication.java
@@ -0,0 +1,13 @@
+package com.vapasi.cicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vapasi/cicdapp/CicdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vapasi.cicdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
